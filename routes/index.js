955d7f291f45475d3eb093cbf9a150597561d5f1
monit1gupta <monitgupta2002@gmail.com>
2023-12-27 01:17:28 +0530

    basic website complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(data);
+  res.render('index.pug', {title:"Latest security news", data_list:data});
 });
 
 module.exports = router;
